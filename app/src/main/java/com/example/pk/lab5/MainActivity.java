8cdb3275214307be802c23c03041f126f9b077ec
zainsajjad22 <zainsajjad22@gmail.com>
2017-07-25 13:50:13 -0700

    Initial commit

---
+++ app/src/main/java/com/example/pk/lab5/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.pk.lab5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.TextureView;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+   Button btn;
+    
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    
+}
